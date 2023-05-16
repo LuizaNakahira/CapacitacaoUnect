fdc545de30c52a4e2c0f85e8759fe2bc8583ed52
Luiza Xavier Nakahira <luizanakahira40@gmail.com>
2023-05-16 16:54:49 -0300

    4

--- script.js
+++ script.js
@@ -160,7 +160,18 @@ adicionar.addEventListener("click", function () {
 
 /*=========================================================================================*/
 
-
+var swiper = new Swiper(".swiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  keyboard: true,
+});
 /*=========================================================================================*/
 
 window.onload = function () {
